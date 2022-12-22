0fe897850f00d94750da47f2672e3e4305dea3aa
mikeycrystal <michaelcrystal87@gmail.com>
2022-12-22 10:04:28 -0800

    First Commit

---
+++ src/SearchForm.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+const SearchForm = ({ searchText }) => {
+  const [text, setText] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    searchText(text);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="e.g. Sports"
+          className="py-1 px-2 rounded-l-lg"
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button
+          type="submit"
+          className="bg-green-400 py-1 px-2 rounded-right-lg text-white"
+        >
+          Search
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchForm;
